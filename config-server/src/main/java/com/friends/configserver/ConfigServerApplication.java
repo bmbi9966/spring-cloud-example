a849afa80114799458b071d58e407042fae1a611
zhangfeng <zhangfeng@magorobot.cn>
2019-07-10 16:24:58 +0800

    2019年7月10日16:24:29

---
+++ config-server/src/main/java/com/friends/configserver/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.friends.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
